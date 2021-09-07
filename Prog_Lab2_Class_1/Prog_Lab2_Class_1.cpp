0497fc83c88ecf70361a1033397eae0c02b91a89
Egor Golikov <20020110ll@gmail.com>
2021-09-07 14:41:50 +0700

    Изменение структуры avto

--- Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
+++ Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
@@ -3,47 +3,53 @@
 using namespace std;
 
 
-struct motor
-{
-    int koni;
-    string tip_topliva;
-    int rasxod;
-    int kol_vo_cilindr;
-};
 
-struct voditel
-{
-    int age;
-    int stag;
-    string pol;
-    string covid_19;
-};
 
-struct kolesa
+struct avto
 {
-    int shirina;
-    int visota;
-    int diametr;
-    string tip_diska;
-};
+    struct har1
+    {
+        int koni;
+        string tip_topliva;
+        int rasxod;
+        int kol_vo_cilindr;
+    } motor;
 
-struct korobka_peredach
-{
-    string tip_korobki;
-    int kolvo_peredach;
-};
+    struct har2
+    {
+        int age;
+        int stag;
+        string pol;
+        string covid_19;
+    } voditel;
 
-struct obchee
-{
-    string tip_privoda;
-    string tip_topliva;
-    int obem_benzobaka;
-    int kolvo_mest;
-    double razgon;
-};
+    struct har3
+    {
+        int shirina;
+        int visota;
+        int diametr;
+        string tip_diska;
+    } kolesa;
 
+    struct har4
+    {
+        string tip_korobki;
+        int kolvo_peredach;
+    } korobka_peredach;
+
+    struct har5
+    {
+        string tip_privoda;
+        string tip_topliva;
+        int obem_benzobaka;
+        int kolvo_mest;
+        double razgon;
+    } obchee;
+};
 
 int main()
 {
+    avto qw;
+    cin >> qw.motor.koni;
     cout << "Hello World!\n";
 }
