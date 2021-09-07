a8f9a34d95d53b5ec9de60efc30b51cb616f34c7
Egor Golikov <20020110ll@gmail.com>
2021-09-07 14:29:18 +0700

    Добавлены структуры для автомобилей

--- Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
+++ Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
@@ -1,16 +1,49 @@
 ﻿#include <iostream>
 
+using namespace std;
 
 
 struct motor
 {
     int koni;
+    string tip_topliva;
     int rasxod;
     int kol_vo_cilindr;
+};
+
+struct voditel
+{
+    int age;
+    int stag;
+    string pol;
+    string covid_19;
+};
+
+struct kolesa
+{
+    int shirina;
+    int visota;
+    int diametr;
+    string tip_diska;
+};
+
+struct korobka_peredach
+{
+    string tip_korobki;
+    int kolvo_peredach;
+};
+
+struct obchee
+{
+    string tip_privoda;
+    string tip_topliva;
+    int obem_benzobaka;
+    int kolvo_mest;
+    double razgon;
 };
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
