43619191773778a47f1cbc3f7152fbfce92ca665
Пользователь <@ASUSPRO>
2021-09-08 05:58:18 +0300

    Добавлены структуры для авто

--- Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
+++ Prog_Lab2_Class_1/Prog_Lab2_Class_1.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+using namespace std;
 
 
 struct motor
@@ -9,7 +9,44 @@ struct motor
     int kol_vo_cilindr;
 };
 
+struct kolesa
+{
+    int shirina;
+    int visota;
+    int diametr;
+};
 
+struct voditel
+{
+    int age;
+    int stag;
+    string pol;
+    string covid_19;
+};
+
+struct korobka_peredach
+{
+    string tip_korobki;
+    int kol_vo_peredach;
+};
+
+struct obchee
+{
+    int massa;
+    string tip_topliva;
+    string tip_privoda;
+    int kolvo_mest;
+    double razgon_do_100;
+};
+
+struct avto
+{
+    motor har1;
+    kolesa har2;
+    voditel har3;
+    korobka_peredach har4;
+    obchee har5;
+};
 int main()
 {
     std::cout << "Hello World!\n";
